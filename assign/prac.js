e593363c62973734bfbbb5b2658cbb77d0f11e8b
adreameryx <mt3178@srmist.edu.in>
2020-08-28 02:21:33 +0530

    Init commit

---
+++ assign/prac.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+fs.writeFileSync('notez.txt','My name is Mahima\n')
+
+fs.appendFileSync('notez.txt','OHHH YAYY\n yess')
---
+++ notes-app/app.js
@@ -0,0 +1,80 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+const { demandOption } = require('yargs')
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command ({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption:true,
+            type: 'string'
+        } ,  
+        body: {
+            describe: 'Essence',
+            demandOption:true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+ 
+//Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption:true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+
+})
+
+//Create list command 
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes..',
+    handler(){
+       notes.listNotes()
+    }
+
+})
+//Create read  command 
+yargs.command({
+    command: 'read',
+    describe: 'Read notes',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
+    }
+
+})
+
+
+
+
+yargs.parse()
+
+
+   
